68d4ef9c6b3411916af98148bc9499799cd8d4a4
a-trillet <a.trillet@gmail.com>
2020-03-27 02:10:08 +0100

    add Tower enemy list + method selectTarget pour déterminer l'ennemi à cibler(le plus proche)

--- src/Tower.java
+++ src/Tower.java
@@ -1,4 +1,25 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Tower {
-    private int speed;
-    private Point origin;
+    private int frequency = 50;
+    private Point centre;
+    private ArrayList<Enemy>enemies = new ArrayList<Enemy>();
+
+    public Tower(Point origin){
+        this.centre = origin;
+    }
+
+    private Enemy selectTarget(){   //Cette fonction renvoit l'ennemi le plus proche du centre de la tour
+        Enemy target = null;
+        Double dist = null;
+        for(Enemy e : enemies){
+            if (target == null | this.centre.distance(e.getOrigin) < dist){
+                target = e;
+                dist = this.centre.distance(e.getOrigin);
+            }
+        }
+        return target;
+    }
 }
