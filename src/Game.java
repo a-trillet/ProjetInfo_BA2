f18f6b91574ac8360ffa4e9f05804959f767bcfb
a-trillet <a.trillet@gmail.com>
2020-04-09 23:12:07 +0200

    classe playscreen

--- src/Game.java
+++ src/Game.java
@@ -65,26 +65,9 @@ public class Game extends Application {
         layout2.setAlignment(Pos.CENTER);
         scene2 = new Scene(layout2, 600, 300);
 
-        // Layout 3 jeu
-        BorderPane borderPane = new BorderPane();
-        StackPane map = new StackPane(); // permet de supperposer les différents éléments de la map (image, tours,..)
-        drawing.drawSquare(new Point(100,100));// à enlever
-
-        map.getChildren().addAll(drawing);
-        GridPane shop = new GridPane();
-        shop.setPadding(new Insets(10,10,10,10));
-        shop.setVgap(8);
-        shop.setHgap(10);
-        Button fireTowerButton = new Button("fire tower");
-        GridPane.setConstraints(fireTowerButton,0,0);
-        Button iceTowerButton = new Button("ice tower");
-        GridPane.setConstraints(iceTowerButton,1,0);
-        shop.getChildren().addAll(fireTowerButton,iceTowerButton);
-
-
-        borderPane.setCenter(map);
-        borderPane.setRight(shop);
-        scene3 =new Scene(borderPane,1000,500);
+        //Layout 3
+        PlayScreen playscreen = new PlayScreen();
+        scene3 =new Scene(playscreen.sceneView(),1000,500);
 
         window.setScene(scene3);
         window.setTitle("Cool Name To Be Inserted Here!!!");
--- src/PlayScreen.java
+++ src/PlayScreen.java
@@ -1,11 +1,35 @@
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 
 public class PlayScreen {
     public static Drawing drawing= new Drawing();
 
-    public BorderPane sceneView(){}
+    public BorderPane sceneView(){
+        // Layout 3 jeu
+        BorderPane borderPane = new BorderPane();
+        StackPane map = new StackPane(); // permet de supperposer les différents éléments de la map (image, tours,..)
+        drawing.drawSquare(new Point(100,100));// à enlever
+
+        map.getChildren().addAll(drawing);
+        GridPane shop = new GridPane();
+        shop.setPadding(new Insets(10,10,10,10));
+        shop.setVgap(8);
+        shop.setHgap(10);
+        Button fireTowerButton = new Button("fire tower");
+        GridPane.setConstraints(fireTowerButton,0,0);
+        Button iceTowerButton = new Button("ice tower");
+        GridPane.setConstraints(iceTowerButton,1,0);
+        shop.getChildren().addAll(fireTowerButton,iceTowerButton);
+
+
+        borderPane.setCenter(map);
+        borderPane.setRight(shop);
+        return borderPane;
+    }
 
 }
