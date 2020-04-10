5a375f4c60e473c928531cfa37a304272d43cfcf
Tomvray <thomas.vray@scarlet.be>
2020-04-10 04:02:43 +0200

    commit

--- src/PlayScreen.java
+++ src/PlayScreen.java
@@ -31,7 +31,7 @@ public class PlayScreen {
         drawing.setOnMouseClicked(e->{if (towerType!=null){new TowerMaker(drawing,towerType,new Point(e.getX(),e.getY()));}});
         //drawing.setOnMouseReleased(new TowerButtonListener(drawing));
         map.setBackground(new Background(new BackgroundFill(Color.rgb(40, 40, 40), CornerRadii.EMPTY, Insets.EMPTY)));
-        drawing.setOnMouseClicked(e->
+        drawing.setOnMouseClicked(e->{if (towerType!=null){new TowerMaker(drawing,towerType,new Point(e.getX(),e.getY()));}});
 
 
 
