0b6c5273de6b5bdd8200f32639c9277370282d90
Tomvray <thomas.vray@scarlet.be>
2020-04-16 03:22:09 +0200

    vérifications si pas sur une autre tour

--- src/FireTower.java
+++ src/FireTower.java
@@ -1,6 +1,6 @@
 public class FireTower extends Tower{
 
-    private static int cost=4;
+    private static int cost=20;
     public FireTower(Point originPoint){
         super(originPoint);
         
--- src/TowerMaker.java
+++ src/TowerMaker.java
@@ -1,31 +1,39 @@
 
 public class TowerMaker {
     private Drawing drawing;
-    public TowerMaker(Drawing d,String type, Point p) {
+
+    public TowerMaker(Drawing d, String type, Point p) {
         this.drawing = d;
         Tower t = null;  //à vérifier
         switch (type) {
             case "FIRE":
-                t = new FireTower(p); break;
-            case "ICE": break;
+                t = new FireTower(p);
+                break;
+            case "ICE":
+                break;
         }
-        PlayScreen.towerType=null;
+        PlayScreen.towerType = null;
         if (t != null && CheckTowerOk(t)) {
             Player.getPlayer().addTower(t);
             drawing.drawSquare(p);
+            Player.getPlayer().addGold(-t.getCost());  //peut etre à bouger
 
-            //tower commence à tirer
+            //tower commence à tirer  / peut etre apas nécessaire
 
             //}
 
         }
     }
-        private boolean CheckTowerOk(Tower to){
-            boolean res = false;
-            if (Player.getPlayer().getGold() >= to.getCost()) {
-                res = true;
-            }
-            return res;
-        }
-    }
 
+    private boolean CheckTowerOk(Tower to) {
+        boolean res = false;
+        if (Player.getPlayer().getGold() >= to.getCost()) {
+            res = true;
+        }
+        for (Tower tower : Player.getPlayer().getTowerList()){
+            if (tower.isOn(to.getCentre())) {
+                res = false;
+            }}
+        return res;
+    }
+}
